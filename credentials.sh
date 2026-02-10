a93508f63ee5f29bbb8dc0478d2c7eeb1691b055
anthonayokh <116527175+anthonayokh@users.noreply.github.com>
2026-02-10 05:30:33 -0800

    Create credentials.sh

---
+++ credentials.sh
@@ -0,0 +1,7 @@
+
+# For Gmail (recommended to use App Password)
+
+# import os 
+export EMAIL_SENDER="your_email@gmail.com"
+export EMAIL_PASSWORD="your_app_password"
+export EMAIL_RECEIVER="receiver@gmail.com"
